2d14c514b737f1d5e06186da4c37342f1d5d89c7
Arthur Cabral <63428806+CabralArthur@users.noreply.github.com>
2022-08-06 20:07:27 -0300

    feat(main-component): doing "challenge" to create MainComponent

--- 1 - first-react/index.js
+++ 1 - first-react/index.js
@@ -1,1 +1,27 @@
-ReactDOM.render(<h1>Oi full</h1>, document.getElementById("root"))
+// => Create a custom React Component
+// => Call it "MainContent"
+// => This component should return a simple h1 element that says "I'm learning React"
+// => Put this component bellow the Navbar
+
+const NavBar = () => {
+    return (
+        <h1>My NavBar</h1>
+    );
+};
+
+const MainComponent = () => {
+    return (
+        <h1>"I'm learning React!"</h1>
+    )
+};
+
+const Wrapper = () => {
+    return (
+        <>
+            <NavBar/>
+            <MainComponent/>
+        </>
+    )
+}
+
+ReactDOM.render(<Wrapper/>, document.getElementById("root"))
